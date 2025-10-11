61100b6ffc39f8536d8a4d63a152a28bc747d0ec
Jolanta Hyla <jola.hyla@onet.pl>
2025-10-11 10:00:30 +0200

    Interpreting program arguments

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -6,11 +6,19 @@ public class World {
         run(args);
         System.out.println("System zakonczyl dzialanie");
     }
+
     public static void run(String[] args) {
-        System.out.println("Zwierzak idzie do przodu");
-        if (args.length > 0) {
-            System.out.println("Przekazane argumenty:");
-            System.out.println(String.join(", ", args));
+        System.out.println("Start");
+
+        for (String arg : args) {
+            switch (arg) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tyłu");
+                case "r" -> System.out.println("Zwierzak skręca w prawo");
+                case "l" -> System.out.println("Zwierzak skręca w lewo");
+            }
         }
+
+        System.out.println("Stop");
     }
 }
