d238aa7d96f76f2f32618798fa24c90e2e43bb16
Jolanta Hyla <jola.hyla@onet.pl>
2025-10-11 11:04:36 +0200

    enum MoveDirection and class OprionsParser added

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,24 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class OptionsParser {
+    public static MoveDirection[] parse(String[] args) {
+        MoveDirection[] temp = new MoveDirection[args.length];
+        int counter = 0;
+
+        for (String arg : args) {
+            switch (arg){
+                case "f" -> temp[counter++]=MoveDirection.FORWARD;
+                case "b" -> temp[counter++]=MoveDirection.BACKWARD;
+                case "r" -> temp[counter++]=MoveDirection.RIGHT;
+                case "l" -> temp[counter++]=MoveDirection.LEFT;
+            }
+        }
+
+        MoveDirection[] moves = new MoveDirection[counter];
+        System.arraycopy(temp, 0, moves, 0, counter);  // create new array in order to get rid of 'null' values at the end of temporary array
+
+        return moves;
+    }
+}
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,24 +1,26 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
-    public static void main(String[] args) {
-        System.out.println("system wystartowal");
-        run(args);
+
+    static void main(String[] args) {
+        System.out.println("System wystartowal");
+        run(OptionsParser.parse(args));
         System.out.println("System zakonczyl dzialanie");
     }
 
-    public static void run(String[] args) {
+    public static void run(MoveDirection[] directions) {
         System.out.println("Start");
 
-        for (String arg : args) {
-            switch (arg) {
-                case "f" -> System.out.println("Zwierzak idzie do przodu");
-                case "b" -> System.out.println("Zwierzak idzie do tyłu");
-                case "r" -> System.out.println("Zwierzak skręca w prawo");
-                case "l" -> System.out.println("Zwierzak skręca w lewo");
+        for (MoveDirection direction : directions) {
+            switch (direction) {
+                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
+                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
+                case LEFT -> System.out.println("Zwierzak skręca w lewo");
             }
         }
-
         System.out.println("Stop");
     }
 }
